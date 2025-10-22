7b0fc77fb91130ac575ad9414a16df2c0a1489a9
Reece Howard <rjhphr@umsystem.edu>
2025-10-22 18:54:04 -0500

    Created base files for project.

---
+++ webservice.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+import os, sys, webbrowser, tempfile
+from datetime import datetime, timedelta
+from typing import Dict, Tuple, List
+import requests
+
+
+if __name__ == "__main__":
+    main()
