8fa4c2581c9c2d5c4e467db3afb6330aabbebc42
Sydney Cable <sydneycable@gmail.com>
2025-10-24 16:25:11 -0500

    added API key function

--- webservice.py
+++ webservice.py
@@ -2,6 +2,18 @@ import requests
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+def get_api_data(symbol, function):
+    api_key = "2O7W9WY18QMH3DXR"
+    if function == "TIME_SERIES_INTRADAY":
+        interval = "5min"
+        url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&apikey={api_key}"
+    else:
+        url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={api_key}"
+    
+    response = requests.get(url)
+    data = response.json()
+    return data
+
 def get_time_series_function(choice):
     functions = {
         "1": "TIME_SERIES_INTRADAY",
