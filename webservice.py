2282d56ca9d4237de2ac9fd110bace4674ec81fe
Sydney Cable <sydneycable@gmail.com>
2025-10-24 15:55:37 -0500

    added imports and made a function for time series

--- webservice.py
+++ webservice.py
@@ -1,9 +1,12 @@
-from __future__ import annotations
-import os, sys, webbrowser, tempfile
-from datetime import datetime, timedelta
-from typing import Dict, Tuple, List
 import requests
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-
-if __name__ == "__main__":
-    main()
+def get_time_series_function(choice):
+    functions = {
+        "1": "TIME_SERIES_INTRADAY",
+        "2": "TIME_SERIES_DAILY",
+        "3": "TIME_SERIES_WEEKLY",
+        "4": "TIME_SERIES_MONTHLY"
+    }
+    return functions.get(choice, None)
