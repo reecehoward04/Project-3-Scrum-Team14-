fc195d46c0447844ac04678d05ce426767de51b5
Sydney Cable <sydneycable@gmail.com>
2025-10-24 16:09:21 -0500

    added chart type function

--- webservice.py
+++ webservice.py
@@ -10,3 +10,10 @@ def get_time_series_function(choice):
         "4": "TIME_SERIES_MONTHLY"
     }
     return functions.get(choice, None)
+
+def get_chart_type(choice):
+    chart_types = {
+        "1": "bar",
+        "2": "line"
+    }
+    return chart_types.get(choice, None)
